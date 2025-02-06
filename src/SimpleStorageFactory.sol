5865da6f986059eacb9cc8f0fd50e4e6098dffe2
vicjuma <vicjuma945@gmail.com>
2025-02-06 17:26:55 +0300

    add functional implementation

--- src/SimpleStorageFactory.sol
+++ src/SimpleStorageFactory.sol
@@ -4,10 +4,21 @@ pragma solidity 0.8.28;
 import {SimpleStorage} from "./SimpleStorage.sol";
 
 contract SimpleStorageFactory {
-    SimpleStorage public simpleStorage;
+    SimpleStorage[] public s_simpleStorageContracts;
 
     function deploySimpleStorageContract(string memory _name, uint8 _favNum) public {
-        simpleStorage = new SimpleStorage(_name, _favNum);
+        SimpleStorage simpleStorage = new SimpleStorage(_name, _favNum);
+        s_simpleStorageContracts.push(simpleStorage);
+    }
+
+    function simpleStorageStore(uint8 _index, string memory _name, uint8 _favNum) public {
+        SimpleStorage simpleStorage = s_simpleStorageContracts[_index];
+        simpleStorage.store(_name, _favNum);
+    }
+
+    function simpleStorageRead(uint8 _index, string memory _name) public view returns (uint8) {
+        SimpleStorage simpleStorage = s_simpleStorageContracts[_index];
+        return simpleStorage.retrieve(_name);
     }
 
 }
